31ed204badc217b00204511cf6d918c4eab41a45
jjfrancez <franz.althea05@gmail.com>
2025-03-10 22:31:40 +0800

    Update CPA1_menu.py

--- CPA1_menu.py
+++ CPA1_menu.py
@@ -9,19 +9,19 @@ def main_menu():
         print("\n")
 
         # Options
-        print("1. Grade School")
-        print("2. Elementary School")
+        print("1. Elementary School")
+        print("2. Junior High School")
         print("3. Senior High School")
         print("4. Exit")
 
         choice = input("Select a level: ")
 
         if choice == '1':
-            print("You selected Grade School. Staring quiz...\n")
+            print("You selected Elementary. Staring quiz...\n")
             import CPA1_ELEM_SECTION
             CPA1_ELEM_SECTION.math_practice_elem()
         elif choice == '2':
-            print("You selected Elemenatry. Starting quiz...\n")
+            print("You selected Junior High School. Starting quiz...\n")
         elif choice == '3':
             print("You selected Senior High School. Starting quiz...\n")
         elif choice == '4':
