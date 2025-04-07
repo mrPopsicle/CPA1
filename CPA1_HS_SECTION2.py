2a2274de50bdee411534647f1b41a3212437ccc4
jjfrancez <franz.althea05@gmail.com>
2025-04-07 19:16:28 +0800

    Update CPA1_HS_SECTION2.py

--- CPA1_HS_SECTION2.py
+++ CPA1_HS_SECTION2.py
@@ -7,6 +7,9 @@ import Algebra_Menu
 
 def math_practice_highschool():
     """Main menu to select a topic for high school math."""
+    score = 0
+    streak = 0
+    
     print("\n Welcome to the High School Math Practice Program!")
 
     while True:
@@ -21,21 +24,18 @@ def math_practice_highschool():
 
         if choice == '1':
             print("You selected Sequences...\n")
-            SEQUENCES_MENU.sequences_menu()
+            score, streak = SEQUENCES_MENU.sequences_menu(score, streak)
         elif choice == '2':
             print("You selected Algebra...\n")
-            Algebra_Menu.algebra_menu()   
+            score, streak = Algebra_Menu.algebra_menu(score, streak)   
         elif choice == '3':
             print("You selected Senior High School. Starting quiz...\n")
-            Trigonometry_Menu.trigonometry_menu()
+            score, streak = Trigonometry_Menu.trigonometry_menu(score, streak)
         elif choice == '4':
-            print("Thank you for playing! Goodbye!")
+            print("Thank you for playing! Your final score is {score}. Goodbye!")
             break
         else:
             print("Invalid choice. Please select a valid option.\n")
-
-
-        
 
 def get_user_input():
     """Get user input and return the value. Accepts integers, floats,"""
@@ -54,6 +54,18 @@ def get_user_input():
 
     
     return user_input
+
+def update_score(correct, streak, score):
+    """Updates score based on streak"""
+    points = 10
+    if streak > 1:
+        points *= 2
+    if correct:
+        score += points
+        streak += 1
+    else:
+        streak = 0
+    return score, streak
 
 
 if __name__ == "__main__":
