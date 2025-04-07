8abb615fa54ae588d14856a0e85010dffa52c1ab
jjfrancez <franz.althea05@gmail.com>
2025-04-07 20:57:22 +0800

    Update CPA1_menu.py

--- CPA1_menu.py
+++ CPA1_menu.py
@@ -1,35 +1,57 @@
 # %%
 def main_menu():
     while True:
-        print("*" * 40)
-        print("*" + " " * 38 + "*")
-        print("*    Welcome to the Math Practice App!    *") # Change name?
-        print("*" + " " * 38 + "*")
-        print("*" * 40)
-        print("\n")
+        print("\n" + "=" * 60)
+        print("||" + " " * 56 + "||")
+        print("||" + " " * 13 + "👾  M A T H   I N V A D E R S  👾" + " " * 13 + "||")
+        print("||" + " " * 56 + "||")
+        print("=" * 60)
 
-        # Options
-        print("1. Elementary School")
-        print("2. High School")
-        print("3. Exit")
+        print(r"""
+                           .
+                           |
+                           ^
+                          / \
+                         /___\
+                        |=   =|
+                        |     |
+                        |     |
+                       /|##!##|\
+                      / |##!##| \
+                     /  |##!##|  \
+                    |  / ^ | ^ \  |
+                    | /  ( | )  \ |
+                    |/   ( | )   \|
+                        ((   ))
+                       ((  :  ))
+                       ((  :  ))
+                        ((   ))
+                         (( ))
+                          ( )
+                           .
+                           .
+                           .
+                    """) 
+        
+        print("              [1] Elementary School")
+        print("              [2] High School")
+        print("              [3] Exit\n")
 
-        choice = input("Select a level: ")
+        choice = input(">> Select a level (1-3): ")
 
         if choice == '1':
-            print("You selected Elementary. Starting quiz...\n")
+            print("\n🚀 Launching Elementary Mission...\n")
             import CPA1_ELEM_SECTION
             CPA1_ELEM_SECTION.math_practice_elem()
         elif choice == '2':
-            print("You selected Junior High School. Starting quiz...\n")
+            print("\n🛸 Launching High School Mission...\n")
             import CPA1_HS_SECTION2
             CPA1_HS_SECTION2.math_practice_highschool()
         elif choice == '3':
-            print("Thank you for playing! Goodbye!")
+            print("\n👋 Game Over. Thanks for playing Math Invaders!")
             break
         else:
-            print("Invalid choice. Please select a valid option.\n")
-
-        
+            print("\n⚠️ Invalid choice. Please select a valid option.\n")
 
 if __name__ == "__main__":
     main_menu()
